56748ef61f51efc77e2cc88e3fdcd9c06a4d2ade
sarasintoyia8-art <sarasintoyia8@gmail.com>
2025-11-19 13:55:53 +0300

    Initial commit for Task3: string length

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char str[] = "Sintoyia";
+    int length = 8;
+
+    printf("You entered: %s\n", str);
+    printf("The length of the string is: %d\n", length);
+
+    return 0;
+}
+
